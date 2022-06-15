71c2ecbc94ccb58f75faaec3608ebd751baab6a7
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-15 17:02:10 -0300

    rename
    I renamed a file to not cause conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { MainContent } from "./components/MainContent";
-import { MapLoad } from "./components/MapLoad/MapLoad";
+import { MapLoad } from "./components/MapSection/MapLoad";
 import { ServiceSection } from "./components/ServiceSection";
 import { Widget } from "./components/Widget";
 
