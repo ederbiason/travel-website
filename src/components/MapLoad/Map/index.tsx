0e6f1d51558a275abb8e2499de3f1a8f687294ad
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-15 15:22:49 -0300

    Google map feature
    Implementing the google map feature

---
+++ src/components/MapLoad/Map/index.tsx
@@ -0,0 +1,43 @@
+import { useMemo, useCallback, useRef } from 'react';
+import { 
+    GoogleMap,
+    GoogleMapProps
+} from '@react-google-maps/api'
+
+import * as C from './styles';
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type MapOptions = google.maps.MapOptions;
+
+export function MapMap() {
+    const mapRef = useRef<GoogleMapProps>();  
+
+    const mapContainerStyle = useMemo(() => ({
+        width: '100%',
+        height: '400px',
+    }), [])
+
+    const center = useMemo<LatLngLiteral>(() => ({lat: -23.427320, lng: -51.936958}), [])
+
+    const options = useMemo<MapOptions>(() => ({
+        disableDefaultUI: true,
+        clickableIcons: false,
+        zoomControl: true,
+    }), [])
+
+    const onLoad = useCallback((map: any) => (mapRef.current = map), [])
+
+    return (
+        <>
+            <C.Title><span>⛰️</span>Eco Adrenaline</C.Title>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+                options={options}
+                onLoad={onLoad}
+            >
+            </GoogleMap>
+        </>
+    )
+}
---
+++ src/components/MapLoad/Map/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    position: absolute;
+    top: .5rem;
+    left: 1rem;
+    color: #000;
+    z-index: 10;
+    margin: 0;
+    padding: 0;
+    font-size: 25px;
+`
